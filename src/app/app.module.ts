4fd035093310fdd72e2f38dce31aa3c54303232f
Samanta Fluture <samantafluture@gmail.com>
2021-08-31 16:21:00 -0300

    create input type file for dropzone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/upload/dropzone/dropzone.component.ts
+++ src/app/upload/dropzone/dropzone.component.ts
@@ -12,4 +12,8 @@ export class DropzoneComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  fileBrowseHandler($event: any) {
+
+  }
+
 }
