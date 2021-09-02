31f4013dbce20cfc3eafca796971df3f8e4b948e
Samanta Fluture <samantafluture@gmail.com>
2021-09-01 22:23:31 -0300

    refact upload method for dropzone component

--- src/app/pages/upload/dropzone/dropzone.component.ts
+++ src/app/pages/upload/dropzone/dropzone.component.ts
@@ -29,17 +29,34 @@ export class DropzoneComponent implements OnInit {
   }
 
   upload() {
-    this.file = this.files[0];
-    this.uploadService.uploadFile(this.file).subscribe(
-      (event: HttpEvent<any>) => {
-        if (event.type === HttpEventType.UploadProgress) {
-          const total = event.total ?? 1;
-          this.uploadProgress = Math.round(100 * (event.loaded / total));
-          console.log('## uploadProgress ## ', this.uploadProgress);
-        }
-      },
-      (error) => console.log(error)
-    );
-    console.log(`## file '${this.file.name}' upload with success ##`);
+    if (this.files) {
+      for (let i = 0; i < this.files.length; i++) {
+        this.uploadService.uploadFile(this.files[i]).subscribe(
+          (event: HttpEvent<any>) => {
+            if (event.type === HttpEventType.UploadProgress) {
+              const total = event.total ?? 1;
+              this.uploadProgress = Math.round(100 * (event.loaded / total));
+              console.log('## uploadProgress ## ', this.uploadProgress);
+            }
+          },
+          (error) => console.log(error)
+        );
+        console.log(`## file upload with success ##`);
+      }
+    }
   }
+
+  //   this.file = this.files[0];
+  //   this.uploadService.uploadFile(this.file).subscribe(
+  //     (event: HttpEvent<any>) => {
+  //       if (event.type === HttpEventType.UploadProgress) {
+  //         const total = event.total ?? 1;
+  //         this.uploadProgress = Math.round(100 * (event.loaded / total));
+  //         console.log('## uploadProgress ## ', this.uploadProgress);
+  //       }
+  //     },
+  //     (error) => console.log(error)
+  //   );
+  //   console.log(`## file '${this.file.name}' upload with success ##`);
+  // }
 }
